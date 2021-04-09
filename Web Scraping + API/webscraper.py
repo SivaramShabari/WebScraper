eb07781e19d09541dc995e386930ef2f51bc1185
SivaramShabari <sivaramsbr1@gmail.com>
2021-04-10 02:00:39 +0530

    r

--- Web Scraping + API/webscraper.py
+++ Web Scraping + API/webscraper.py
@@ -30,23 +30,23 @@ for i in range(20):
     print(id)
 
 
-# pos = 1
-# max = 20
-# while True:
-#     if keyboard.is_pressed('esc'):
-#         break
-#     elif keyboard.is_pressed('right'):
-#         if pos<20:
-#             pos=pos+1
-#         else:
-#             pos=1
-#     elif keyboard.is_pressed('left'):
-#         if pos>0:
-#             pos=pos-1
-#         else:
-#             pos=20 
-#     im = cv2.imread('Output/{}.jpg'.format(pos),cv2.IMREAD_ANYCOLOR)
-#     cv2.imshow(str(pos),im)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+pos = 1
+max = 20
+while True:
+    if keyboard.is_pressed('esc'):
+        break
+    elif keyboard.is_pressed('right'):
+        if pos<20:
+            pos=pos+1
+        else:
+            pos=1
+    elif keyboard.is_pressed('left'):
+        if pos>0:
+            pos=pos-1
+        else:
+            pos=20 
+    im = cv2.imread('Output/{}.jpg'.format(pos),cv2.IMREAD_ANYCOLOR)
+    cv2.imshow(str(pos),im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
---
+++ readme.md
@@ -0,0 +1,16 @@
+Download the chromium driver dependeing on the version of Google Chrome you have
+
+For the web scraper program, install selenium ,opencv, keyboard, pymongo, datetime via pip
+
+
+
+For the API you should have Node JS installed. 
+        To run the API go into the API directory, cmd " npm init" and it installs all the required dependencies
+	Once you run the index.js file in API directory an API endpoint will be created on localhost port 3003 -- 
+	https://localhost:3003
+
+Contact me if you really wanna execute everything in this repo... LoL
+
+
+There is no CRUD operation here
+Only read from DB :P
